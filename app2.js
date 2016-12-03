85b193ce0d2e2b3d914ea81240bad25cf4c071bb
Nguyen Hoang Anh Khoi <anhkhoi.nguyenhoang@gmail.com>
2016-12-03 15:54:39 -0800

    commit

--- app2.js
+++ app2.js
@@ -18,12 +18,18 @@
     const ulList =  document.getElementById('list');
 
     // Create references
-    const dbRefList = firebase.database().ref().child('object');
+    const dbRefObject = firebase.database().ref().child('object');
+    //const dbRefList = dbRefObject.child('user2');
 
     // Sync object changes
-    dbRefList.on('child_added', function() {
-        console.log(snap.val())
+    dbRefObject.on('value', function(){
+        pre0Object.innerText = JSON.stringify(snap.val(), null, 3);
     });
+
+    // Sync list changes
+    //dbRefList.on('child_added', function() {
+    //    console.log(snap.val())
+    //});
 
 
 }()());
