a45a37f42e9fa6f3da19b892e97038132dd1b04d
Nguyen Hoang Anh Khoi <anhkhoi.nguyenhoang@gmail.com>
2016-12-03 13:57:46 -0800

    Commit

--- action_page.js
+++ action_page.js
@@ -5,5 +5,14 @@ var UsernameDir ={};
 
 function UsernameCheck() {
     var x= document.getElementById("Sign-Up").elements[0].value;
-    UsernameDir.push(x);
+    var i;
+    for(var i = 0 ; i < UsernameDir.length ; i++){
+        var item = UsernameDir.item(i);
+        if (x == item){
+            return "Username already taken"
+        }
+        else {
+            UsernameDir.push(x);
+        }
+    }
 }
