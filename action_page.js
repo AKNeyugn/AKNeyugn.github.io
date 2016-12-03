f036f7dae59badad3624a5e5adfdfde93487096f
Nguyen Hoang Anh Khoi <anhkhoi.nguyenhoang@gmail.com>
2016-12-03 13:49:01 -0800

    Commit

--- action_page.js
+++ action_page.js
@@ -1,3 +1,9 @@
 /**
  * Created by anhkh_000 on 03-Dec-16.
  */
+var UsernameDir ={};
+
+function UsernameCheck() {
+    var x= document.getElementById("Sign-Up").elements[0].value;
+    UsernameDir.push(x);
+}
