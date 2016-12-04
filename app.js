61c32180d5396f937a975bb015242a5ecb7320ff
Kamal <kdeowra5@gmail.com>
2016-12-03 17:27:33 -0800

    Merge branch 'master' of https://github.com/AKNeyugn/AKNeyugn.github.io
    # Conflicts:
    #	.idea/workspace.xml

--- app.js
+++ app.js
@@ -3,6 +3,44 @@
  */
 (function() {
 
+    var https = require('https');
+
+    var data = JSON.stringify({
+        api_key: 'API_KEY',
+        api_secret: 'API_SECRET',
+        to: '16047608577',
+        from: '441632960961',
+        text: 'Hello from Nexmo'
+    });
+
+    var options = {
+        host: 'rest.nexmo.com',
+        path: '/sms/json',
+        port: 443,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(data)
+        }
+    };
+
+    var req = https.request(options);
+
+    req.write(data);
+    req.end();
+
+    var responseData = '';
+    req.on('response', function(res){
+        res.on('data', function(chunk){
+            responseData += chunk;
+        });
+
+        res.on('end', function(){
+            console.log(JSON.parse(responseData));
+        });
+    });
+
+
 // Initialize Firebase
     const config = {
         apiKey: "AIzaSyChbZiYz7onNOz55ZWA4S3XdUhKOUQW1W4",
@@ -62,42 +100,6 @@
     */
 
     // Sends a text
-    var https = require('https');
-
-    var data = JSON.stringify({
-        api_key: 'API_KEY',
-        api_secret: 'API_SECRET',
-        to: '16047608577',
-        from: '441632960961',
-        text: 'Hello from Nexmo'
-    });
-
-    var options = {
-        host: 'rest.nexmo.com',
-        path: '/sms/json',
-        port: 443,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': Buffer.byteLength(data)
-        }
-    };
-
-    var req = https.request(options);
-
-    req.write(data);
-    req.end();
-
-    var responseData = '';
-    req.on('response', function(res){
-        res.on('data', function(chunk){
-            responseData += chunk;
-        });
-
-        res.on('end', function(){
-            console.log(JSON.parse(responseData));
-        });
-    });
 
 
 }()());
