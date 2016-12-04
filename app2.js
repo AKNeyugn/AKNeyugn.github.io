2c0d9eb3a4e5301283ba1d5a5e5ff1aead5cc159
Nguyen Hoang Anh Khoi <anhkhoi.nguyenhoang@gmail.com>
2016-12-03 16:13:35 -0800

    commit

--- app2.js
+++ app2.js
@@ -1,37 +1,15 @@
 /**
  * Created by anhkh_000 on 03-Dec-16.
  */
-(function() {
+function writeUserData(){
 
-    /*
-// Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyChbZiYz7onNOz55ZWA4S3XdUhKOUQW1W4",
-        authDomain: "textme-a2a10.firebaseapp.com",
-        databaseURL: "https://textme-a2a10.firebaseio.com",
-        storageBucket: "textme-a2a10.appspot.com",
-        messagingSenderId: "220797746951"
-    };
-    firebase.initializeApp(config);
-    */
+    //Get elements
+    const txtEmail = document.getElementById('txtEmail');
+    const txtPassword = document.getElementById('txtPassword');
 
-    // Get elements
-    const pre0Object = document.getElementById('object');
-    const ulList =  document.getElementById('list');
-
-    // Create references
-    const dbRefObject = firebase.database().ref().child('object');
-    //const dbRefList = dbRefObject.child('user2');
-
-    // Sync object changes
-    dbRefObject.on('value', function(){
-        pre0Object.innerText = JSON.stringify(snap.val(), null, 3);
+    firebase.database().ref().set({
+        username: txtEmail,
+        password: txtPassword
     });
 
-    // Sync list changes
-    //dbRefList.on('child_added', function() {
-    //    console.log(snap.val())
-    //});
-
-
-});
+};
